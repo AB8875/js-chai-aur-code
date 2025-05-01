ef36753b8b8c827b9721d1095b451f486dcc5d94
AB8875 <akswami2300@gmail.com>
2025-05-01 11:24:30 +0000

    array

---
+++ 01_firstDay/string.js
@@ -0,0 +1,17 @@
+const name="Abhishek" 
+const projectsCount =45 
+// console.log(`My name is ${name}  and i had made ${projectsCount} projects.`); //use backtics not sinlgeupper commas
+
+// console.log(name[1]);
+// console.log(name.toUpperCase());
+
+// console.log(name.charAt(2));
+// console.log(name.indexOf('e'));
+const newString= name.substring(0,5)
+// console.log(newString) // when we make substrign so the index we gave ex(0,5) in this index 5 not included.
+
+
+let whatToday= new Date()
+// console.log(whatToday.toDateString());
+console.log(whatToday.toLocaleDateString());
+
---
+++ normal-2/array.js
@@ -0,0 +1,10 @@
+ const myArr=[0,1,2,3,4,5,]
+
+ const myHeros=["kilvis","shaitanpari","kansmama","kaliyanag"]
+
+ const myArr2=new Array(1,2,3,4,5)
+
+// console.log(myArr[0]);
+
+myArr.push(7) //Adding elements in Last of an array
+console.log(myArr);
