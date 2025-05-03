fe27fa0f5d8c47be5ca1892c99de7ef24c47e9d8
AB8875 <akswami2300@gmail.com>
2025-05-03 11:23:30 +0000

    ttttt

--- normal-2/array.js
+++ normal-2/array.js
@@ -6,5 +6,24 @@
 
 // console.log(myArr[0]);
 
-myArr.push(7) //Adding elements in Last of an array
-console.log(myArr);
+// myArr.push(7) //Adding elements in Last of an array
+ 
+
+// myArr.pop() // for removing last element in our array
+
+// const newArr=myArr.join() //basically .join converts an array to string
+
+// console.log(newArr);
+
+// console.log(myArr);
+
+
+/* slice or splice very important */
+
+// const myNew1=myArr.slice(1,3)
+// console.log(myNew1);
+//array.slice have ranges first starting and last ending. ending range not included in results
+
+ let  spArr=[0,1,2,3,4,5,6]
+let myNew2=spArr.splice(1,2,20)
+console.log(myNew2);
